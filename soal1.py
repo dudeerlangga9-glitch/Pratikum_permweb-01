cce2a2665251c0f560a7354822df9762289c2986
dudeerlangga9-glitch <dudeerlangga9@gmail.com>
2025-11-19 10:59:09 +0700

    Add files via upload

---
+++ soal1.py
@@ -0,0 +1,6 @@
+def celcius_ke_fahrenreit (celcius):
+    f= (celcius*9/5)+32
+    return f
+
+print(celcius_ke_fahrenreit(0))
+print(celcius_ke_fahrenreit(100))
---
+++ soal4.py
@@ -0,0 +1,6 @@
+def bilangan(angka):
+    for i in range(1,angka):
+        if i % 2 != 0:
+            print(i)
+
+bilangan(20)
