b88d5dfa0fa0fa8f0061dc17a1c43dbd7a8256e3
dudeerlangga9-glitch <dudeerlangga9@gmail.com>
2025-11-26 13:41:42 +0700

    Add files via upload

---
+++ import panggilan.py
@@ -0,0 +1,11 @@
+import panggilan
+
+print('hasil dari kubus',panggilan.kubus(4))
+
+print('hasil dari balok',panggilan.balok(2, 3, 4))
+
+print('hasil dari prisma',panggilan.prisma(4, 5, 6))
+
+print('haisl dari tabung',panggilan.tabung(3,8))
+
+print('hasil dari kerucut',panggilan.kerucut(3,7))
